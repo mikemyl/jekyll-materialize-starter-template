63c4d0d9db6964f7c0ac8090a56ba6cb55ffb1a7
sestus <michail.milonakis@gmail.com>
2018-07-29 17:44:04 +0100

    Add slider

--- js/my.js
+++ js/my.js
@@ -1,4 +1,7 @@
-// Navigation Menu DropDown
+/******************************
+ **** Dropdown Navigation *****
+ ******************************/
+
 
 options= {hover: true, coverTrigger: false}
 
@@ -14,4 +17,14 @@ document.addEventListener('DOMContentLoaded', function() {
 document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.sidenav');
   var instances = M.Sidenav.init(elems, mobilePptions);
+});
+
+
+/*****************************
+********** Slider ************
+******************************/
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.slider');
+  var instances = M.Slider.init(elems, options);
 });
